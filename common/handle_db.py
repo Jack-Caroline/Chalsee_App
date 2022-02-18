ddec31d2859f245f34007cf50077d51c2f664d34
金少开 <jack.jin@chalsee.com>
2022-02-18 10:05:53 +0800

    完成第一步公共方法模块   excel表格处理   数据库处理

--- common/handle_db.py
+++ common/handle_db.py
@@ -2,3 +2,31 @@
 # -*- coding: UTF-8 -*-
 # Filename:jinxiaojian
 # Create Time: 2022/2/17 12:16
+import pymssql
+
+
+class DB:
+    def __init__(self):
+        """
+        初始化数据库，连接数据库信息
+        创建游标对象
+        """
+        self.con = pymssql.connect(
+            host="10.251.251.101",
+            user="uatlink",
+            password="ABC.123"
+        )
+        self.cur = self.con.cursor()
+
+    def find_data(self, sql):
+        self.cur.execute(sql)
+        res = self.cur.fetchall()
+        return res
+
+    def updata_data(self, sql):
+        self.cur.execute(sql)
+        self.con.commit()
+
+    def close_data(self):
+        self.cur.close()
+        self.con.close()
--- common/handle_excel.py
+++ common/handle_excel.py
@@ -16,14 +16,47 @@ class Excel:
         self.sheet_name = sheet_name
 
     def open(self):
+        """
+        打开 excel 表格中文件，表单
+        :return:
+        """
         self.wb = openpyxl.load_workbook(self.filename)
         self.sh = self.wb[self.sheet_name]
 
     def read_excel(self):
-        pass
+        """
+        读取表单中数据，将数据处理为可数据驱动的格式
+        读取第一行表头
+        依次读取其他行数据，然后和第一行表头进行 zip 打包
+        :return:
+        """
+        self.open()
+        res = list(self.sh.rows)
+        title = []
+        case_datas = []
+        for t in res[0]:
+            title.append(t.value)
+        for row in res[1:]:
+            data = []
+            for v in row:
+                data.append(v.value)
+            case = dict(zip(title, data))
+            case_datas.append(case)
+        return case_datas
 
     def write_excel(self, row, column, value):
+        """
+        写入数据
+        :param row: 行
+        :param column: 列
+        :param value: 数据值
+        :return:
+        """
         self.sh.cell(row=row, column=column, value=value)
 
     def save_excel(self):
+        """
+        保存 Excel 表格
+        :return:
+        """
         self.wb.save(self.filename)
